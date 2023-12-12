5c5cc32f50c9afde93f231178abc5cef5fdc24a2
budayyy <budibuday97@gmail.com>
2023-12-12 11:58:03 +0700

    menambahkan route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Project from "./Project";
+import Contact from "./Contact";
+import Stack from "./Stack";
+import NotFound from "./NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/projects",
+    element: <Project />,
+  },
+  {
+    path: "/stacks",
+    element: <Stack />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
